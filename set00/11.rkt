136b9224bddcb9d62264e3beeec8ad75a207fe1a
chenzex <chenzex@ccs.neu.edu>
2013-09-16 16:07:09 -0400

    struct definition EX11~15

---
+++ set00/11.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;; contracts for the posn-functions
+;; make-posn : Number Number -> Posn
+;; posn-x : Posn -> Number
+;; posn-y : Posn -> Number
+;; posn? : any -> boolean
